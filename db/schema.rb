2917d07595aec218d3ca3e7aaf82da2feb3658a2
Carlos Muiño <camumino@gmail.com>
2016-07-04 19:01:37 +0200

    added avatar to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622190858) do
+ActiveRecord::Schema.define(version: 20160704165603) do
 
   create_table "contacts", force: :cascade do |t|
-    t.string   "name",         limit: 255
-    t.string   "surname",      limit: 255
-    t.string   "email",        limit: 255
-    t.integer  "contact_type", limit: 4
-    t.integer  "user_id",      limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "name",                limit: 255
+    t.string   "surname",             limit: 255
+    t.string   "email",               limit: 255
+    t.integer  "contact_type",        limit: 4
+    t.integer  "user_id",             limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "contacts", ["email"], name: "index_contacts_on_email", unique: true, using: :btree
